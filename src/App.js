5bf300eaa5b026a8ba39eb7c2c71ad5d182269d2
khjang <z_gahu@naver.com>
2023-02-11 20:41:53 +0900

    05-3 생명주기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import React from "react";
 
 class App extends React.Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    console.log("hello");
+  }
+
+  componentDidMount() {
+    console.log("component rendered");
+  }
+
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
+
+  componentWillUnmount() {
+    console.log("Goodbye, cruel world");
+  }
+  state = { count: 0 };
+
+  add = () => {
+    this.setState((current) => ({ count: this.state.count + 1 }));
+  };
+
+  minus = () => {
+    this.setState((current) => ({ count: this.state.count - 1 }));
+  };
+
   render() {
-    return <h1>I'm a class Component</h1>;
+    console.log("render");
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
   }
 }
 
