156733e72d56dac058f96004ac54fefb6854dbbe
khjang <z_gahu@naver.com>
2023-02-17 07:47:03 +0900

    07 영화 앱 다듬기

--- src/Movie.js
+++ src/Movie.js
@@ -2,14 +2,23 @@ import React from "react";
 import PropType from "prop-types";
 import "./Movie.css";
 
-function Movie({ title, year, summary, poster }) {
+function Movie({ title, year, summary, poster, genres }) {
   return (
-    <div class="movie">
+    <div className="movie">
       <img src={poster} alt={title} title={title} />
-      <div class="movie__data">
-        <h3 class="movie__title">{title}</h3>
-        <h5 class="movie__year">{year}</h5>
-        <p class="movie__summary">{summary}</p>
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="movie_genres">
+          {genres.map((genre, index) => {
+            return (
+              <li key={index} className="movie__genre">
+                {genre}
+              </li>
+            );
+          })}
+        </ul>
+        <p className="movie__summary">{summary}</p>
       </div>
     </div>
   );
@@ -20,6 +29,7 @@ Movie.propTypes = {
   title: PropType.string.isRequired,
   summary: PropType.string.isRequired,
   poster: PropType.string.isRequired,
+  genres: PropType.arrayOf(PropType.string).isRequired,
 };
 
 export default Movie;
