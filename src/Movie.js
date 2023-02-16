a8865b4d81a325d04872f706933054336534ac7f
khjang <z_gahu@naver.com>
2023-02-16 21:30:58 +0900

    06-4 영화 앱 스타일링 하기

--- src/Movie.js
+++ src/Movie.js
@@ -1,12 +1,21 @@
 import React from "react";
 import PropType from "prop-types";
+import "./Movie.css";
 
-function Movie({ id, title, year, summary, poster }) {
-  return <h4>{title}</h4>;
+function Movie({ title, year, summary, poster }) {
+  return (
+    <div class="movie">
+      <img src={poster} alt={title} title={title} />
+      <div class="movie__data">
+        <h3 class="movie__title">{title}</h3>
+        <h5 class="movie__year">{year}</h5>
+        <p class="movie__summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
 
 Movie.propTypes = {
-  id: PropType.number.isRequired,
   year: PropType.number.isRequired,
   title: PropType.string.isRequired,
   summary: PropType.string.isRequired,
